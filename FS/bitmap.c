6b29e98a107e0144423019862022f45fdb744241
PanK0 <panici.k@gmail.com>
2019-03-05 19:41:55 +0100

    trying to do something good with bitmap.c - not complete

--- FS/bitmap.c
+++ FS/bitmap.c
@@ -1,23 +1,56 @@
 #include bitmap.h
 
 // converts a block index to an index in the array,
-// and a uint8_t that indicates the offset of the bit inside the array
+// and a uint8_t that indicates the offset of the bit inside the array.
 BitMapEntryKey BitMap_blockToIndex(int num) {
+	BitMapEntryKey entry_key;
+	entry_key.entry_num = num / NUMBITS;
+	entry_key.bit_num = num % NUMBITS;
+	return entry_key;
 	
 }
 
 // converts a bit to a linear index
 int BitMap_indexToBlock(int entry, uint8_t bit_num) {
+	int num = (entry * NUMBITS) + bit_num;
+	return num;
+}
+
+// returns the pos of the first bit equal to status in number num
+// returns -1 in case of bit not found
+int BitMap_check(uint8_t num, int status) {
+	if (num < 0) return ERROR_RESEARCH_FAULT;
+	int i = 7;
+	while (i >= 0) {
+		if (status != 0 && (num & (1 << i))) {
+			return i;			
+		}
+		else if (status == 0 && !(num & (1 << i))) {
+			return i;
+		}	
 	
+		--i;
+	}
+	return ERROR_RESEARCH_FAULT;
 }
 
 // returns the index of the first bit having status "status"
 // in the bitmap bmap, and starts looking from position start
 int BitMap_get(BitMap* bmap, int start, int status) {
-	
+	if (start < 0 || start >= bmap->num_bits) return ERROR_RESEARCH_FAULT;
+	int i = start;
+	while (i < bmap->num_bits) {
+		int pos = BitMap_check((bmap->entries)[i], status);
+		if (pos != ERROR_RESEARCH_FAULT) return i + (7 - pos);
+	}
+	return ERROR_RESEARCH_FAULT;
 }
 
 // sets the bit at index pos in bmap to status
+//FUNZIONA SOLO CON STATUS = 1
 int BitMap_set(BitMap* bmap, int pos, int status) {
-	
+	if (pos < 0 || pos >= bmap->numbits) return ERROR_RESEARCH_FAULT;
+	int array_index = pos / NUMBITS;
+	int offset = pos % NUMBITS;
+	(bmap->entries)[array_index] ^= (1 << offset);
 }
--- FS/bitmap.h
+++ FS/bitmap.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <stdint.h>
 
+// Errors
+#define ERROR_RESEARCH_FAULT	-1
+
 //Just to keep in mind
 #define OCCUPIED	1
 #define FREE		0
+#define NUMBITS		8
 
 typedef struct {
   int num_bits;
@@ -22,9 +26,18 @@ BitMapEntryKey BitMap_blockToIndex(int num);
 // converts a bit to a linear index
 int BitMap_indexToBlock(int entry, uint8_t bit_num);
 
+// returns the pos of the first bit equal to status in number num
+// returns -1 in case of bit not found
+int BitMap_check(uint8_t num, int status);
+
 // returns the index of the first bit having status "status"
 // in the bitmap bmap, and starts looking from position start
 int BitMap_get(BitMap* bmap, int start, int status);
 
 // sets the bit at index pos in bmap to status
 int BitMap_set(BitMap* bmap, int pos, int status);
+
+
+/*	NOTES
+*	Changed char with uint8_t to avoid mistakes
+*/
