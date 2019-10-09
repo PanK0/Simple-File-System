48095a54ec156e247fb93f57e7f8e8c2ae426d7e
PanK0 <panici.k@gmail.com>
2019-10-09 20:03:19 +0200

    Shell now works better

--- Tests/simplefs_test.c
+++ Tests/simplefs_test.c
@@ -287,38 +287,44 @@ int main (int argc, char** argv) {
 		
 		printf (BOLD_RED "\n* * * * TESTING BY SHELL * * * *\n" COLOR_RESET);
 		
+		size_t len = MAX_CMD_LEN;
+		
 		char cmd1[MAX_CMD_LEN] = "";
 		char cmd2[MAX_CMD_LEN] = "";
-		int ret = -1;
+		char* line = NULL;
 		
-		while (strcmp(cmd1, "quit") != 0) {
+		int ret = -1;
+			
+		while (-TBA) {
 			printf (BOLD_YELLOW "g@g:~%s " COLOR_RESET, dirhandle->dcb->fcb.name);
-			scanf("%s %s", cmd1, cmd2);
+			
+			getline(&line, &len, stdin);
+			sscanf(line, "%s %s", cmd1, cmd2);
 			
 			// * * * GENERAL CMDs * * *
-			if (strcmp(cmd1, SYS_SHOW) == 0) {
+			if (strcmp(cmd1, SYS_SHOW) == 0 ){
 				SimpleFS_print(&fs, dirhandle);
 			}
 			else if (strcmp(cmd1, SYS_HELP) == 0) {
 				
 				printf (YELLOW " GENERAL\n" COLOR_RESET
-				SYS_SHOW" .     : show status of File System\n"
-				SYS_HELP" .       : show list of commands\n"
+				SYS_SHOW"       : show status of File System\n"
+				SYS_HELP"         : show list of commands\n"
 				DIR_REMOVE" [obj]     : removes the object named 'obj'\n"
 				YELLOW "\n DIR\n" COLOR_RESET
-				DIR_SHOW" .      : show actual directory info\n"
+				DIR_SHOW"        : show actual directory info\n"
 				DIR_CHANGE" [dir]     : goes into directory named 'dir'\n"
 				DIR_MAKE" [dir]  : creates a directory named 'dir'\n"
-				DIR_LS" .         : prints the dir's content\n"
+				DIR_LS"           : prints the dir's content\n"
 				YELLOW "\n FILE\n" COLOR_RESET
-				FILE_SHOW" .  : show the last opened file\n"
+				FILE_SHOW"    : show the last opened file\n"
 				FILE_MAKE" [fil]   : create a file named 'fil' \n"
 				FILE_MAKE_N" [n]    : create n files\n"
 				FILE_OPEN" [fil]   : open file named 'fil' \n"
 				FILE_WRITE" [txt]  : writes 'txt' in the last opened file\n"
 				FILE_SEEK" [n]    : moves the cursor at pos n in the opened file\n"
 				
-				FILE_CLOSE" .     : closes the last opened file\n"
+				FILE_CLOSE"       : closes the last opened file\n"
 				
 				);
 			}
@@ -412,7 +418,7 @@ int main (int argc, char** argv) {
 			}
 			
 			// read a file
-			else if (strcmp(cmd1, FILE_READ) == 0) {
+			else if (strcmp(cmd1, FILE_READ) == 0 && filehandle != NULL) {
 				int cmd_len = (BLOCK_SIZE-sizeof(FileControlBlock) - sizeof(BlockHeader) ) + ((BLOCK_SIZE-sizeof(BlockHeader))/sizeof(int) * (filehandle->fcb->fcb.size_in_blocks-1));
 				char text[cmd_len];
 				ret = SimpleFS_read(filehandle, text, cmd_len);
@@ -424,6 +430,13 @@ int main (int argc, char** argv) {
 				filehandle = NULL;
 			}
 			
+			// Quit
+			else if (strcmp(cmd1, "quit") == 0) {
+				printf (YELLOW "Shell exited with return status %d\n" COLOR_RESET, ret);
+				break;
+			}
+			
+			// Wrong command
 			else {
 				printf (YELLOW "UNKNOWN COMMAND - Type 'help .' for command list\n" COLOR_RESET);
 			}
@@ -431,7 +444,7 @@ int main (int argc, char** argv) {
 		}
 	
 	ret = 0;
-	printf (YELLOW "Shell exited with return status %d\n" COLOR_RESET, ret);
+	
 	}
 	
 	DiskDriver_flush(&disk);
